91a7148d5f148c91ca57815c16634f21d0efe617
Matt Staples <itms2@lunet.lboro.ac.uk>
2019-01-18 11:00:47 +0000

    v0015 - backup before iteminventorypuzzle import

---
+++ Assets/Fencer/Scripts/HideRoom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideRoom : MonoBehaviour {
+
+   // public float alphaColour;
+	// Use this for initialization
+	private Renderer renderer;
+    private Color newColor;
+    private bool roomEntered = false;
+
+    void Awake () {
+        renderer = gameObject.GetComponent<Renderer>();
+        newColor = renderer.material.color;
+        newColor.a = 1f;
+        renderer.material.color = newColor;
+	}
+	
+    void OnTriggerEnter2D(Collider2D coll){
+        roomEntered = true;
+    }
+
+    void Update(){
+        if(roomEntered == true){
+            newColor = renderer.material.color;
+            newColor.a -= 0.1f;
+            renderer.material.color = newColor;
+                
+        }
+
+        if (newColor.a <= 0f){
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Fencer/Scripts/Stairs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Stairs : MonoBehaviour {
+
+    public GameObject player;
+    private InputState inputState;
+    BoxCollider2D boxColl;
+
+	// Use this for initialization
+void Awake () {
+    boxColl = gameObject.GetComponent<BoxCollider2D> ();
+    inputState = player.GetComponent<InputState>();
+}
+	
+	// Update is called once per frame
+	void Update () {
+ 
+        if(inputState.absVelY > 1 || Input.GetKey("down")){ //was zero originally, cures jumping bug
+            boxColl.isTrigger = true;
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D coll){
+        boxColl.isTrigger = false;
+    }
+
+		
+	
+}
